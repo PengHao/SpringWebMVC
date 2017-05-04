d29067dcfe5d71e8f3d6d1c78919614333684e97
彭浩 <wolfpeng.ph@alibaba-inc.com>
2017-05-04 09:53:06 +0800

    set Project to SubProjects

---
+++ HelloWebMVC/Web/src/main/java/com/wolfpeng/controller/Index.java
@@ -0,0 +1,29 @@
+package com.wolfpeng.controller;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by penghao on 2017/4/25.
+ * Copyright ? 2017Äê Alibaba. All rights reserved.
+ */
+
+@Controller
+@RequestMapping("/home")
+public class Index {
+    private static final Logger logger = LoggerFactory.getLogger(Index.class);
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(HttpServletRequest request) {
+        logger.debug("handle index request");
+        request.setAttribute("date", new Date());
+        return "index";
+    }
+}
